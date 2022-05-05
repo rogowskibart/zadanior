61e699b43d138aef8be3111fa84fafe82f1326db
Bartosz Rogowski <rogowskibart@gmail.com>
2022-05-06 00:07:28 +0200

    added TasksDao.kt

--- app/src/main/java/com/rogowskibart/zadanior/database/dao/TasksDao.kt
+++ app/src/main/java/com/rogowskibart/zadanior/database/dao/TasksDao.kt
@@ -1,4 +1,19 @@
 package com.rogowskibart.zadanior.database.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.rogowskibart.zadanior.model.dto.TaskDto
+
+@Dao
 interface TasksDao {
+
+    @Insert
+    fun insert(taskDto: TaskDto)
+
+    @Query("DELETE FROM taskdto WHERE id = :taskId")
+    fun delete(taskId: Long)
+
+    @Query("SELECT * FROM taskdto")
+    fun getAll(): List<TaskDto>
 }
